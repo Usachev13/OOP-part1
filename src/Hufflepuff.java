c6fcd39b2bb0dba1229b16adddfbb24cc15b7dae
Usachev13 <2893318@gmail.com>
2023-09-17 19:32:39 +0300

    starts hw

---
+++ src/Hufflepuff.java
@@ -0,0 +1,35 @@
+public class Hufflepuff {
+    private int hardworking;
+    private int loyal;
+    private int honest;
+
+    public Hufflepuff(int hardworking, int loyal, int honest) {
+        this.hardworking = hardworking;
+        this.loyal = loyal;
+        this.honest = honest;
+    }
+
+    public int getHardworking() {
+        return hardworking;
+    }
+
+    public void setHardworking(int hardworking) {
+        this.hardworking = hardworking;
+    }
+
+    public int getLoyal() {
+        return loyal;
+    }
+
+    public void setLoyal(int loyal) {
+        this.loyal = loyal;
+    }
+
+    public int getHonest() {
+        return honest;
+    }
+
+    public void setHonest(int honest) {
+        this.honest = honest;
+    }
+}
